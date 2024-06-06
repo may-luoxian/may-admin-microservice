50c5a253822a260764ed01a7ef4dfd2e6192e96a
sunyk <2876053639@qq.com>
2024-06-07 00:14:57 +0800

    项目初始化，nacos环境搭建

---
+++ management-center/src/main/java/com/may/managementcenter/ManagementCenterApplication.java
@@ -0,0 +1,16 @@
+package com.may.managementcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 系统管理中心
+ */
+@SpringBootApplication
+public class ManagementCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementCenterApplication.class, args);
+    }
+
+}
