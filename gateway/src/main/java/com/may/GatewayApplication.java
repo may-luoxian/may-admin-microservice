5ffd21642c258701013d70e6070f1851b0ba1337
sunyk <2876053639@qq.com>
2024-06-10 02:15:50 +0800

    功能新增：新增gateway网关负载均衡

--- gateway/src/main/java/com/may/GatewayApplication.java
+++ gateway/src/main/java/com/may/GatewayApplication.java
@@ -1,11 +1,10 @@
-package com.may.gateway;
+package com.may;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GatewayApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
--- management-center/src/main/java/com/may/ManagementCenterApplication.java
+++ management-center/src/main/java/com/may/ManagementCenterApplication.java
@@ -1,4 +1,4 @@
-package com.may.managementcenter;
+package com.may;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
