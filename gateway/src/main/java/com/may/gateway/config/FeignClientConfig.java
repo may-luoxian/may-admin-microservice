c2d6149ab4efee2a2f568136a1100b8081868a05
sunyk <2876053639@qq.com>
2024-06-27 23:18:00 +0800

    功能新增：新增身份认证服务调用

--- gateway/src/main/java/com/may/gateway/config/FeignClientConfig.java
+++ gateway/src/main/java/com/may/gateway/config/FeignClientConfig.java
@@ -3,7 +3,7 @@ package com.may.gateway.config;
 import com.may.gateway.client.AuthClient;
 import org.springframework.context.annotation.Configuration;
 import reactivefeign.spring.config.EnableReactiveFeignClients;
-
+//test
 @Configuration(proxyBeanMethods = false)
 @EnableReactiveFeignClients(clients = {AuthClient.class})
 public class FeignClientConfig {
